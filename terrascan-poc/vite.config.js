ceadf20fbc05598476e855068c2a94748169ab92
AvoirJoseph <balut399@gmail.com>
2026-05-20 16:00:28 +0800

    base path wrong

--- terrascan-poc/vite.config.js
+++ terrascan-poc/vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/terrascanpoc/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
